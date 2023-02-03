3066151e71bca57b44893196be017b7f78a4576f
JingWei-S <weijing.524@gmail.com>
2023-02-03 11:55:15 +1100

    add the add tic function

--- script.js
+++ script.js
@@ -3,18 +3,6 @@ const gameBoard = (() => {
     let tics = ['X', 'X', 'X', 'O', 'O', 'X', 'O', 'O', 'O'];   // this is to store the tic tocs
     const container = document.querySelector(".container");
     const draw = () => {
-        // for (let i = 0; i < 3; i++) {
-        //     const row = document.createElement('div');
-        //     row.classList.add('row');
-        //     for (let j = 0; j < 3; j++) {
-        //         const cube = document.createElement('div');
-        //         cube.classList.add('cube');
-        //         cube.style.width = LEN / 3 + 1 + "px";
-        //         cube.style.height = LEN / 3 + 1 +"px";
-        //         row.appendChild(cube);
-        //     }
-        //     container.append(row);
-        // }
         for (let i = 0; i < 9; i++) {
             const cube = document.createElement('div');
             cube.classList.add("cube");
@@ -28,14 +16,34 @@ gameBoard.draw();
 
 // build the module for display control
 const displayControl = (() => {
+    const container = document.querySelector(".container");
     const add = (tics) => {
-
+        // const cubes = container.querySelector(".cube:nth-child(2)");
+        // console.log(tics[2]);
+        // cubes.textContent = tics[2];
+        // add all the dummy tics to the game board
+        for (let i = 0; i < 9; i++) {
+            const cube = container.querySelector(`.cube:nth-child(${i+1})`);
+            // console.log(tics[i]);
+            cube.textContent = tics[i];
+        }
     }
+    return {add}
 })();
 
+displayControl.add(gameBoard.tics);
 // build the player factory function 
 const Player = (name, side) => {
-    return {name, side}
+    const getName = () => name;
+    // addTic - add tic to the board and update the board
+    const addTic = () => {
+        const cubes = document.querySelectorAll(".cube");
+        cubes.forEach(cube => cube.addEventListener("click", (e) => {
+            console.log(side)
+            e.target.textContent = side;
+        }));
+    };
+    return {getName, addTic}
 };
 
 const player1 = Player('Jing', '😈');
