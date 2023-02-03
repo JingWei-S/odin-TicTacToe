a9a3ab341cb2cc82264ec19619d9d846885899a4
JingWei-S <weijing.524@gmail.com>
2023-02-03 11:13:34 +1100

    update the board implementation

--- script.js
+++ script.js
@@ -1,26 +1,37 @@
 // build the gameBoard as module
 const gameBoard = (() => {
+    let tics = ['X', 'X', 'X', 'O', 'O', 'X', 'O', 'O', 'O'];   // this is to store the tic tocs
     const container = document.querySelector(".container");
-    const LEN = 480;
     const draw = () => {
-        for (let i = 0; i < 3; i++) {
-            const row = document.createElement('div');
-            row.classList.add('row');
-            for (let j = 0; j < 3; j++) {
-                const cube = document.createElement('div');
-                cube.classList.add('cube');
-                cube.style.width = LEN / 3 + 1 + "px";
-                cube.style.height = LEN / 3 + 1 +"px";
-                row.appendChild(cube);
-            }
-            container.append(row);
+        // for (let i = 0; i < 3; i++) {
+        //     const row = document.createElement('div');
+        //     row.classList.add('row');
+        //     for (let j = 0; j < 3; j++) {
+        //         const cube = document.createElement('div');
+        //         cube.classList.add('cube');
+        //         cube.style.width = LEN / 3 + 1 + "px";
+        //         cube.style.height = LEN / 3 + 1 +"px";
+        //         row.appendChild(cube);
+        //     }
+        //     container.append(row);
+        // }
+        for (let i = 0; i < 9; i++) {
+            const cube = document.createElement('div');
+            cube.classList.add("cube");
+            container.appendChild(cube);
         }
     }
-    return {draw}
+    return {draw, tics}
 })();
 // draw the board
 gameBoard.draw();
 
+// build the module for display control
+const displayControl = (() => {
+    const add = (tics) => {
+
+    }
+})();
 
 // build the player factory function 
 const Player = (name, side) => {
